e61980a2d9a54530657469cb2a0f6e83fdd98c0f
floriangoedel <florian.goedel@htl.rennweg.at>
2019-05-06 16:59:15 +0200

    Added Testing-Scene; Added new Sprites; Interacting method works

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    public bool inCollider;
+    public bool leverActive = false;
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        inCollider = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        inCollider = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && inCollider)
+        {
+            if (leverActive)
+            {
+                leverActive = false;
+                Debug.Log("The Lever was de-activated!");   
+            }
+            else
+            {
+                leverActive = true;
+                Debug.Log("The Lever was activated!");  
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public bool inCollider;
+    public bool leverActive = false;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        inCollider = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        inCollider = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && inCollider)
+        {
+            if (leverActive)
+            {
+                leverActive = false;
+                Debug.Log("The Lever was de-activated!");   
+            }
+            else
+            {
+                leverActive = true;
+                Debug.Log("The Lever was activated!");  
+            }
+        }
+    }
+}
