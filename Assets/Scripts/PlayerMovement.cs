b1d7881d5e9740caf453442f6104de981c288842
Nicolas Stocker <nicolas.stocker@htl.rennweg.at>
2019-05-13 15:24:32 +0200

    Sound Effects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public float movementSpeed;
     private PlayerMovement instance;
-    
+
+    private bool isMoving = false;
+    public AudioSource footsteps;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +23,33 @@ public class PlayerMovement : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+
+        footsteps = GetComponent<AudioSource>();
     }
     
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed;
+
+        if (rb.velocity.x != 0 || rb.velocity.y != 0)
+        {
+            isMoving = true;
+            
+            
+        }
+        else isMoving = false;
+
+        if (isMoving)
+        {
+            if (!footsteps.isPlaying)
+            {
+                footsteps.Play();
+            }
+        }
+       
+        
+        
     }
 
 }
