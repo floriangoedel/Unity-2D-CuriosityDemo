12dbc40571f97631b6e14aa077459113d090fbc3
floriangoedel <florian.goedel@htl.rennweg.at>
2019-05-13 17:45:39 +0200

    Updated everything

---
+++ Assets/Scripts/TafelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TafelController : MonoBehaviour
+{
+    // functional variables
+    public bool inCollider;
+
+    // lines
+    public string[] lines;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        inCollider = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        inCollider = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && inCollider)
+        {
+            TextBoxManager.instance.ShowText(lines);
+        }
+    }
+}
